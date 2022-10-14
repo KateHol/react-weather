d1f5839e23c869b705f948f0c3e733824c52b7d8
KateHol <golotakaterina@gmail.com>
2022-10-14 21:56:17 +0300

    add weather js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
-    Weather App
+      <div className='container'>
+      <h1>Weather App</h1>
+      <Weather />
+
+   <footer>This project was coded by Kate Holota and is{" "}
+    <a href="https://github.com/KateHol/weather" 
+    target="_blank"
+    rel='noreferrer'>
+       open-sourced on GitHub
+      </a> and
+      <a href="https://helpful-caramel-ab529a.netlify.app"> hosted on Netlify</a>
+    </footer> 
+    </div>
     </div>
   );
 }
