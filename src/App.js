44c49819c313419cdcb10d2e0cdfaa2f6212b6fe
KateHol <golotakaterina@gmail.com>
2022-10-16 14:45:45 +0300

    add search engine

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-      <h1>Weather App</h1>
-      <Weather />
+        <Weather  defaultCity = "Kyiv"/>
 
    <footer>This project was coded by Kate Holota and is{" "}
     <a href="https://github.com/KateHol/weather" 
