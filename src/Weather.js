3bcaf4761a9b797b7d6bf3f9985dbf768268a488
KateHol <golotakaterina@gmail.com>
2022-10-16 20:51:16 +0300

    change style

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import axios from "axios"
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+
 
 export default function Weather(props) {
     const [weatherData, setWeatherData]= useState({ready: false}); 
@@ -55,6 +57,7 @@ return (
         </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
         </div>
 );
     } else {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,11 +17,9 @@ export default function WeatherInfo(props){
                 <div className="clearfix">
                 <div className="float-left">
                     <WeatherIcon code={props.data.icon} />
-                    </div>
-                     <div className="float-left">
                     <span className="temperature">{Math.round(props.data.temperature)}</span>
-                    <span className="unit">°C</span>
-                    </div>
+                    <span className="unit">°C</span> 
+                                        </div>
                     </div>
                     </div>
                     <div className="col-6">
@@ -31,7 +29,8 @@ export default function WeatherInfo(props){
                 </ul>
             </div>
         </div>
-    </div>
+        </div>
+    
        
     );
 }
