a34b6ab6e1d301cebe96dec36940f2908d2a165c
KateHol <golotakaterina@gmail.com>
2022-10-16 22:01:23 +0300

    add forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props){
             <div className="col-6">
                 <div className="clearfix">
                 <div className="float-left">
-                    <WeatherIcon code={props.data.icon} />
+                    <WeatherIcon code={props.data.icon}  size={52}/>
                     <span className="temperature">{Math.round(props.data.temperature)}</span>
                     <span className="unit">°C</span> 
                                         </div>
